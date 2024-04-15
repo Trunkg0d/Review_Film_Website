000ac4d3bbb64f615efd7487d3f5be8acc5bcd6d
Trunkg0d <trungsanglong@gmail.com>
2024-04-15 22:00:35 +0700

    init: Init project with README and users model

---
+++ main.py
@@ -0,0 +1,39 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+from database.connection import Settings
+from routes.events import event_router
+from routes.users import user_router
+
+app = FastAPI()
+
+settings = Settings()
+
+# register origins
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Register routes
+
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
+
+
+@app.on_event("startup")
+async def init_db():
+    await settings.initialize_database()
+
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/docs/")

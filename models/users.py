50444510c581272fcd52677707a3dc62335e5a64
Trunkg0d <trungsanglong@gmail.com>
2024-04-16 22:06:03 +0700

    update: add more fields for user model

--- models/users.py
+++ models/users.py
@@ -1,11 +1,15 @@
 from beanie import Document
-
+from typing import Optional, List
 from pydantic import BaseModel, EmailStr
+from models.movies import Movie
 
 
 class User(Document):
     email: EmailStr
     password: str
+    img: str
+    role: int   # 0: normal user, 1: admin
+    wish_list: Optional[List[Movie]]
 
     class Settings:
         name = "users"
@@ -14,10 +18,24 @@ class User(Document):
         schema_extra = {
             "example": {
                 "email": "fastapi@packt.com",
-                "password": "strong!!!"
+                "password": "strong!!!",
+                "img": "/static/img.png",
+                "role": 0,
+                "wish_list": [
+                                {
+                                  "_id": "5eb7cf5a86d9755df3a6c593",
+                                  "creator": "string",
+                                  "title": "string",
+                                  "image": "string",
+                                  "description": "string",
+                                  "tags": [
+                                    "string"
+                                  ],
+                                  "location": "string"
+                                }
+                              ]
             }
         }
-
 
 class TokenResponse(BaseModel):
     access_token: str

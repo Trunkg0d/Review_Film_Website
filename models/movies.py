98b6ee1a96f303408ecc3f826b5aa8bb1512e2d1
Trunkg0d <trungsanglong@gmail.com>
2024-05-06 15:44:10 +0700

    add: add release date and director to movies model

--- models/movies.py
+++ models/movies.py
@@ -2,6 +2,7 @@ from typing import Optional, List
 from beanie import Document
 from pydantic import BaseModel
 from celebrities import Celebrity
+from datetime import datetime
 
 class Movie(Document):
     creator: Optional[str]
@@ -9,8 +10,10 @@ class Movie(Document):
     backdrop_path: str
     poster_path: str
     description: str
+    release_date: datetime
     language: str
     tags: List[str]
+    director: List[Celebrity]
     runtime: int # minutes
     average_rating: float
     casts: Optional[List[Celebrity]]
@@ -37,7 +40,9 @@ class MovieUpdate(BaseModel):
     backdrop_path: Optional[str]
     poster_path: Optional[str]
     description: Optional[str]
+    release_date: Optional[datetime]
     tags: Optional[List[str]]
+    director: Optional[List[Celebrity]]
     language: Optional[str]
     runtime: Optional[int]  # minutes
     average_rating: Optional[float]

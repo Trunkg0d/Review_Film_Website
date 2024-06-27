01449237675917e072aa8697893d5dcf15fe3dfb
tngbao1211 <cutebao948@gmail.com>
2024-06-27 17:18:46 +0700

    fix: frontend
    Chatbot window size fix, banner content fix, personal page fix review container

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -35,7 +35,7 @@ function Banner() {
     return () => clearInterval(interval); // Clean up on component unmount
   }, [movies]);
 
-  const bannerMovie = movies[bannerMovieIndex];
+  const bannerMovie = movies[17];
   return (
     (bannerMovie && // Only render banner if there is a banner movie
     <div 
